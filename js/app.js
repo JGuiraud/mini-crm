fab9dcf9e25c918f77766cdeaef7df206f42eb55
JGuiraud <jerome31guiraud@gmail.com>
2016-12-30 15:36:49 +0100

    exercice done

--- js/app.js
+++ js/app.js
@@ -31,4 +31,55 @@ var customers = [
     }
 ]
 
-// Write you're code here. Enjoy !
+
+
+ for (var i = 0; i < customers.length; i++) {
+
+    var divhtml = document.createElement("div");
+    var tartine = document.getElementById("main").appendChild(divhtml);
+    divhtml.classList.add('card');
+    divhtml.innerHTML = 
+                "<h3>" + customers[i].first_name + " " + 
+                customers[i].last_name + "</h3>" + 
+                customers[i].company + ", " + 
+                customers[i].role + "<br>" +
+                customers[i].phone + "<br>" +
+                customers[i].email + "<br>"+"<br>" +
+                customers[i].description 
+     } 
+
+
+
+
+
+// var customer1 = customers[0];
+// var customer2 = customers[1];
+// var customer3 = customers[2];
+
+
+// document.getElementById('user1').innerHTML = 
+// customers[0].first_name + " " + 
+// customers[0].last_name + "<br>" +
+// customers[0].company + ", " + 
+// customers[0].role + "<br>" +
+// customers[0].phone + "<br>" +
+// customers[0].email + "<br>" +
+// customers[0].description 
+
+// document.getElementById('user2').innerHTML = 
+// customers[1].first_name + " " + 
+// customers[1].last_name + "<br>" +
+// customers[1].company + ", " + 
+// customers[1].role + "<br>" +
+// customers[1].phone + "<br>" +
+// customers[1].email + "<br>" +
+// customers[1].description
+
+// document.getElementById('user3').innerHTML = 
+// customers[2].first_name + " " + 
+// customers[2].last_name + "<br>" +
+// customers[2].company + ", " + 
+// customers[2].role + "<br>" +
+// customers[2].phone + "<br>" +
+// customers[2].email + "<br>" +
+// customers[2].description 
